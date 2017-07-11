e51647e9da0aa68ede7fbf824710bbfcc4805ab8
Nivv Oudit <nivvoudit@gmail.com>
2017-07-11 15:12:53 -0500

    Final commit
    Fixed twitter api

--- keys.js
+++ keys.js
@@ -1,8 +1,15 @@
 console.log('this is locked and loaded');
 
 exports.twitterKeys = {
+<<<<<<< Updated upstream
   consumer_key: '',
   consumer_secret: '',
   access_token_key: 	'',
   access_token_secret: '',
+=======
+  consumer_key: '7rg3vICxXv4J6IBIglIQtr0Xq',
+  consumer_secret: 'GJoqY7R5KXX9ODxbuGeCj8PUQmSPvmrlF1yRSYB9h5MpVBy73a',
+  access_token_key: 	'14091637-ECxOxPaNk8s0R9vkSc9mOrGFZdQFSAdpB3wTgzeDu',
+  access_token_secret: 'yZvKAT4eJn63Glb2pBItdN28dxuUTCLWK4V4ogf1HmnYz',
+>>>>>>> Stashed changes
 }
--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 console.log("LIRI node-bot is ready to rumble");
 
 
@@ -85,4 +86,156 @@ if(argument === "do-what-it-says"){
 }
 function outputText(){
     fs.appendFile('log.txt', 'Argument: ' + argument + '. Movie or Song Title: ' + value + '. Movie or Song info: ' + dataText + '.');
+=======
+'use strict';
+
+var Twitter = require('twitter');
+var spotify = require('spotify')
+var SpotifyWebApi = require('spotify-web-api-node');
+var request = require("request");
+var inquirer = require('inquirer');
+var twitterKeyFile = require("./keys.js");
+var spotifyApi = new SpotifyWebApi();
+var fs = require("fs");
+
+var client = new Twitter({
+    consumer_key: twitterKeyFile.twitterKeys.consumer_key,
+    consumer_secret: twitterKeyFile.twitterKeys.consumer_secret,
+    access_token_key: twitterKeyFile.twitterKeys.access_token_key,
+    access_token_secret: twitterKeyFile.twitterKeys.access_token_secret
+});
+
+var whatToDo = [
+{
+    type: "list",
+    message: "pick one",
+    name: "thisIsWhatToDo",
+    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+}];
+
+inquirer.prompt(whatToDo).then(function(whatTheyWannaDo) {
+    switchAction(whatTheyWannaDo.thisIsWhatToDo)
+});
+
+function switchAction(decision) {
+    var title = "";
+    if (decision === "do-what-it-says") {
+        recordIt("doing what it says");
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            var dataArr = data.split(",");
+            decision = dataArr[0];
+            title = dataArr[1];
+
+            callbackGuts(title);
+        });
+    }
+    else {
+        title = getTitle(callbackGuts);
+    }
+    function callbackGuts(input){
+        if (typeof input === "string"){
+            title = input;
+        }
+        else {
+            title = input.title001;
+        }
+        switch (decision) {
+            case "my-tweets" :
+                if (title === ""){
+                    title = "nivvoudit";
+                }
+                getTheTweets(title);
+            break;
+
+            case "spotify-this-song" :
+                if (title === ""){
+                    title = "the sign ace of base";
+                }
+                getTheSongInfo(title);
+            break;
+
+            case "movie-this" :
+                if (title === ""){
+                    title = "mr nobody";
+                }
+                getTheMovieInfo(title);
+            break;
+        }
+    }
+
+}
+
+var getTheTitle = [
+    {
+		type: "input",
+		message: "For what account/song/movie?",
+		name: "title001"
+    }];
+
+function getTitle(callbackGuts) {
+    inquirer.prompt(getTheTitle).then(callbackGuts)
+}
+
+function getTheTweets(account) {
+    var params = {screen_name: account};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        var i=0;
+        if (!error) {
+            recordIt("\n----------------------------");
+            recordIt("\nThe last 5 tweets for @" + account + " are: \n");
+            do{
+                recordIt(i+1 + ". " + tweets[i].text);
+                recordIt("\ntweeted on " + tweets[i].created_at);
+                i++;
+
+                recordIt("\n");
+            } while (i<5);
+        }
+    });
+}
+
+function getTheSongInfo(song) {
+    spotifyApi.searchTracks(song).then(function (data) {
+        var checking = data.body.tracks.items[0].album.artists[0].name;
+            recordIt("\n----------------------------");
+            recordIt("\nThis is the artist: ");
+            recordIt(data.body.tracks.items[0].album.artists[0].name);
+            recordIt("\nThis is the song's name: ");
+            recordIt(data.body.tracks.items[0].name);
+            recordIt("\nThis is preview link of the song from Spotify: ");
+            recordIt(data.body.tracks.items[0].preview_url);
+            recordIt("\nThis is album that the song is from: ");
+            recordIt(data.body.tracks.items[0].album.name);
+        }).catch(function(err) {
+        recordIt('\nUnfortunately, something has gone wrong.', err.message);
+    });
+}
+
+function getTheMovieInfo(movie) {
+
+    var url = "http://www.omdbapi.com/?t=" + movie;
+    request(url, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+        recordIt("\n----------------------------");
+        recordIt("\nThe movie's title is " + JSON.parse(body).Title);
+        recordIt("\nThe movie came out in " + JSON.parse(body).Year);
+        recordIt("\nThe movie's IMDB rating is " + JSON.parse(body).imdbRating);
+        recordIt("\nThe movie's was made in " + JSON.parse(body).Country);
+        recordIt("\nThe movie was filmed in " + JSON.parse(body).Language);
+        recordIt("\nThe plot is: " + JSON.parse(body).Plot);
+        recordIt("\nThe stars of the movie are " + JSON.parse(body).Actors);
+        recordIt("\nFind out more on the website: " + JSON.parse(body).Website);
+  }
+});
+}
+
+function recordIt(stuffToWrite) {
+    console.log(stuffToWrite);
+    fs.appendFile("log.txt", stuffToWrite, function (err) {
+        if (err) {
+            return console.log("Something went wrong writing to log.txt: " + err);
+        }
+    });
+>>>>>>> Stashed changes
 }
